bb1c6e5fda8574f802f68eaa245151f1f96c5dc6
BharathKumarReddy <nbkumar2103@gmail.com>
2025-06-13 17:33:54 +0530

    updated .tf file

--- workspaces/variables.tf
+++ workspaces/variables.tf
@@ -40,10 +40,10 @@ variable "ami_id" {
   description = "AMI ID of joindevops RHEL9"
 }
 
-# variable "instance_type" {
-#     default = {
-#         dev = "t3.micro"
-#         prod = "t3.small"
-#     }
+variable "instance_type" {
+    default = {
+        dev = "t3.micro"
+        prod = "t3.small"
+    }
     
 }
