3ce7bd952db4de2891487ea4be82fdad89ac908a
BharathKumarReddy <nbkumar2103@gmail.com>
2025-07-22 06:06:37 +0530

    updated file

--- tfvars/variables.tf
+++ tfvars/variables.tf
@@ -42,7 +42,7 @@ variable "cidr_blocks" {
 variable "ami_id" {
   type        = string
   default     = "ami-09c813fb71547fc4f"
-  description = "AMI ID of joindevops RHEL9"
+  description = "AMI ID of bharathdevops RHEL9"
 }
 
 variable "instance_type" {
